78c0608bda1d5e9fdbd299c0c8a2b4bdc59602c6
DeeZyJune <127945275+DeeZyJune@users.noreply.github.com>
2025-03-20 12:37:25 +0700

    created controller, model, and route

---
+++ app/Http/Controllers/ProductsController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\Products;
+
+class ProductsController extends Controller
+{
+    public function index()
+  {
+    $products = Products::all();
+    return view('products.index', compact('products'));
+  }
+  /**
+   * Store a newly created resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @return \Illuminate\Http\Response
+   */
+  public function store(Request $request)
+  {
+    $request->validate([
+      'title' => 'required|max:255',
+      'body' => 'required',
+    ]);
+    Products::create($request->all());
+    return redirect()->route('posts.index')
+      ->with('success', 'Post created successfully.');
+  }
+  /**
+   * Update the specified resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function update(Request $request, $id)
+  {
+    $request->validate([
+      'title' => 'required|max:255',
+      'body' => 'required',
+    ]);
+    $post = Products::find($id);
+    $post->update($request->all());
+    return redirect()->route('posts.index')
+      ->with('success', 'Post updated successfully.');
+  }
+  /**
+   * Remove the specified resource from storage.
+   *
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function destroy($id)
+  {
+    $post = Products::find($id);
+    $post->delete();
+    return redirect()->route('posts.index')
+      ->with('success', 'Post deleted successfully');
+  }
+  // routes functions
+  /**
+   * Show the form for creating a new post.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function create()
+  {
+    return view('posts.create');
+  }
+  /**
+   * Display the specified resource.
+   *
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function show($id)
+  {
+    $post = Products::find($id);
+    return view('posts.show', compact('post'));
+  }
+  /**
+   * Show the form for editing the specified post.
+   *
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function edit($id)
+  {
+    $post = Products::find($id);
+    return view('posts.edit', compact('post'));
+  }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductsController;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/products', [ProductsController::class, '']) ->title('products.index');
+
