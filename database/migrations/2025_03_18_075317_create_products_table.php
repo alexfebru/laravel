600928c1c643e5debccd13574706698ae03b8f20
DeeZyJune <127945275+DeeZyJune@users.noreply.github.com>
2025-03-20 12:12:08 +0700

    run the migrated table

--- database/migrations/2025_03_18_075317_create_products_table.php
+++ database/migrations/2025_03_18_075317_create_products_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->decimal('price');
+            $table->string('image');
+            $table->string('category');
             $table->timestamps();
         });
     }
